ac5104180603f972ff89d4e845ef354353ca110e
Alexander Johnston <Aliics@hotmail.com>
2020-11-11 05:15:48 +1300

    repl arg and parser setup

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+)
 
+func main() {
+	repl := flag.Bool("r", false, "interactive REPL mode")
+	flag.Parse()
+	if !*repl {
+		log.Fatalln("REPL mode is currently the only supported mode")
+	}
+	parser := newParser(*repl)
+	if err := parser.parse(); err != nil {
+		log.Fatalln(err)
+	}
 }
